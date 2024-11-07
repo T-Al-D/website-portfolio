15a923bf279a86022350e7534995e009b2bd7f6e
T-Al-D <tahaaldailami2010@hotmail.de>
2024-11-07 17:53:30 +0100

    added images, added Icon to ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,13 @@
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+
 export default function ErrorPage() {
 	return (
 		<div>
 			<pre className="basic-text">
 				Sorry, but this page doesn´t exist!
+			</pre>
+			<pre className="basic-text">
+				<SentimentVeryDissatisfiedIcon></SentimentVeryDissatisfiedIcon>
 			</pre>
 		</div>
 	);
